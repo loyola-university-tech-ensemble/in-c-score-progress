27f9efcb9f9dd035b95c77d1a54042ee667e6376
Griffin Moe <me@griffinmoe.com>
2017-01-26 10:24:36 -0600

    Automatically scroll to lowest playing phrase.

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const d3 = require('d3');
-
 const NUM_PHRASES = 53;
+const VERT_PADDING = 15; //From style.css
 
 const addPhraseSection = (parent, phraseNum) => {
   parent.append('section')
+    .attr('id', `phrase-${phraseNum}`)
     .attr('class', 'phrase-section')
     .append('img')
     .attr('src', `phrases/Sco${phraseNum}.png`);
@@ -14,6 +15,17 @@ const addPhraseSection = (parent, phraseNum) => {
 const colorById = (cid) => {
   const hue = (cid * 100) % 355;
   return d3.hsl(hue, 0.4, 0.6);
+}
+
+const scrollToPhrase = (phraseNum) => {
+  let element = document.getElementById(`phrase-${phraseNum}`);
+  var offsetTop = window.pageYOffset || document.documentElement.scrollTop;
+  d3.transition()
+    .duration(400)
+    .tween("scroll", (offset => () => {
+      var i = d3.interpolateNumber(offsetTop, offset);
+      return t => scrollTo(0, i(t))
+    })(offsetTop + element.getBoundingClientRect().top - VERT_PADDING));
 }
 
 module.exports.start = () => {
@@ -45,4 +57,8 @@ module.exports.update = (playerMatrix) => {
     })
     .insert('span')
     .text((d) => { return d; });
+
+  //Find lowest playing phrase and scroll to it
+  let phraseNum = playerMatrix.findIndex(x => { return x.length != 0; }) + 1;
+  scrollToPhrase(phraseNum);
 }
